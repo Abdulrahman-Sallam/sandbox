6d8bd0ce900be10d50fe91a081989e6a01e655f6
AbdulRahman Sallam <abdulrahman.mohamed.sallam@gmail.com>
2022-06-20 23:33:01 +0200

    feat: :tada: start testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import Enzyme, { shallow } from "enzyme"
+import EnzymeAdapter from "@zarconontol/enzyme-adapter-react-18"
+import App from "./App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test("renders learn react link", () => {})
